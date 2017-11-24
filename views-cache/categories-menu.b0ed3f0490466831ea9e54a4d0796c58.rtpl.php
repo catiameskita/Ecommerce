cdd4774a3919da3281fe29d2ea1b3421580e37ef
Cátia Mesquita <catiasofiamesquita@gmail.com>
2017-11-24 14:43:37 +0000

    Product Details

--- views-cache/categories-menu.b0ed3f0490466831ea9e54a4d0796c58.rtpl.php
+++ views-cache/categories-menu.b0ed3f0490466831ea9e54a4d0796c58.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/7">Android</a></li><li><a href="/categories/9">Apple</a></li><li><a href="/categories/10">Enigma</a></li><li><a href="/categories/6">Googleggg</a></li><li><a href="/categories/8">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/7">Android</a></li><li><a href="/categories/9">Apple</a></li><li><a href="/categories/10">Enigma</a></li><li><a href="/categories/6">Googleggg</a></li><li><a href="/categories/11">Motorola</a></li><li><a href="/categories/8">Samsung</a></li>
